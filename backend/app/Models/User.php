6fae9d000c0f42fa81386b20189d47ebbd530f2a
Sampad Singha <sampadsingha8@gmail.com>
2025-07-30 12:36:41 +0600

    small changes

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -71,27 +71,6 @@ class User extends Authenticatable
         // Merge the two relationships and remove duplicates (based on user id)
         return $friendsInitiated->union($friendsReceived)->distinct()->get();
     }
-//    public function friends()
-//    {
-//        return $this->belongsToMany(User::class, 'friends', 'user_id', 'friend_id')
-//            ->wherePivot('status', 'accepted')
-//            ->orWherePivot('status', 'accepted')
-//            ->where(function($query) {
-//                $query->where('user_id', $this->id)
-//                    ->orWhere('friend_id', $this->id);
-//            })
-//            ->withTimestamps()
-//            ->get();
-//    }
-//    public function pendingSentRequests()
-//    {
-//        return $this->hasMany(Friend::class, 'user_id')
-//            ->where('status', 'pending');
-//    }
-//    public function pendingReceivedRequests()
-//    {
-//        return $this->hasMany(Friend::class, 'friend_id')
-//            ->where('status', 'pending');
-//    }
+
 
 }
