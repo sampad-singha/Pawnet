aeff3ecd0e494b52772c75784545ee7e666bb1ad
Sampad Singha <sampadsingha8@gmail.com>
2025-08-06 12:00:41 +0600

    Added model logics

--- backend/app/Models/Util/Region.php
+++ backend/app/Models/Util/Region.php
@@ -3,8 +3,31 @@
 namespace App\Models\Util;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Region extends Model
 {
-    //
+    protected $table = 'regions';
+
+    protected $fillable = [
+        'name',
+        'translations',
+        'flag',
+        'wikiDataId',
+    ];
+
+    protected $casts = [
+        'translations' => 'array',
+        'flag' => 'boolean',
+    ];
+
+    public function countries(): hasMany
+    {
+        return $this->hasMany(Country::class, 'region_id');
+    }
+
+    public function subregions(): hasMany
+    {
+        return $this->hasMany(Subregion::class, 'region_id');
+    }
 }
--- backend/app/Models/Util/Subregion.php
+++ backend/app/Models/Util/Subregion.php
@@ -3,8 +3,34 @@
 namespace App\Models\Util;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Subregion extends Model
 {
-    //
+    protected $table = 'subregions';
+
+    protected $fillable = [
+        'name',
+        'translations',
+        'region_id',
+        'flag',
+        'wikiDataId',
+    ];
+
+    protected $casts = [
+        'translations' => 'array',
+        'flag' => 'boolean',
+    ];
+
+    public function region(): BelongsTo
+    {
+        return $this->belongsTo(Region::class, 'region_id');
+    }
+
+    public function countries(): hasMany
+    {
+        return $this->hasMany(Country::class, 'subregion_id');
+    }
+
 }
